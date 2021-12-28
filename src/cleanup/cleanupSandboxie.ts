6ef8187bef233cf312b31097f347dbe5eb2239ad
Vladislav <vladislav.puzzyrev@gmail.com>
2021-12-28 14:54:06 +0500

    fix: Указана кодировка для чтения Sandboxie.ini

--- src/cleanup/cleanupSandboxie.ts
+++ src/cleanup/cleanupSandboxie.ts
@@ -2,7 +2,7 @@ import path from 'path';
 import fs from 'fs-extra';
 
 export const cleanupSandboxie = async (): Promise<void> => {
-  const config = await fs.readFile(path.join(__dirname, '../utils/Sandboxie.ini'));
+  const config = await fs.readFile(path.join(__dirname, '../utils/Sandboxie.ini'), 'utf-16le');
   await fs.writeFile('C:/Windows/Sandboxie.ini', config);
   fs.removeSync('C:/Sandbox');
 };
