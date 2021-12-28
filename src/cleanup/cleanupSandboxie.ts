39115b1b0387726aae84475453d9673f2570019f
Vladislav <vladislav.puzzyrev@gmail.com>
2021-12-28 14:57:48 +0500

    fix: Указана кодировка для записи Sandboxie.ini

--- src/cleanup/cleanupSandboxie.ts
+++ src/cleanup/cleanupSandboxie.ts
@@ -3,6 +3,6 @@ import fs from 'fs-extra';
 
 export const cleanupSandboxie = async (): Promise<void> => {
   const config = await fs.readFile(path.join(__dirname, '../utils/Sandboxie.ini'), 'utf-16le');
-  await fs.writeFile('C:/Windows/Sandboxie.ini', config);
+  await fs.writeFile('C:/Windows/Sandboxie.ini', config, { encoding: 'utf-16le' });
   fs.removeSync('C:/Sandbox');
 };
